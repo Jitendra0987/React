793fc7b9cc758ebf2596fa15c95e343e97499bfb
Jitendra <jitendra01432@gmail.com>
2024-10-04 10:56:43 +0530

    usestate increment decrement

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,30 @@
 import { useState } from "react";
 
 const App=()=>{
-  const [name, setName]=useState("najneen")
-  
-  const myName=()=>{
-    setName("gouri")
-  }
-  return(
-    <>
-   <h1>wellcome to use status</h1>
-   <h1>my name : {name}</h1>
-   <button onClick={myName}>change name</button>
-    </>
-  )
+const [cnt, setCnt]=useState(0);
+const myDcrement =()=>{
+
+    if(cnt<=1)
+    {
+        alert("No decrese minus value")
+    }
+
+    else
+    {
+        setCnt(cnt-1)
+    }
+}
+
+    return(
+        <>
+        <center>
+       <h1>counter App</h1>
+       <h1>count : {cnt}</h1>
+       <button onClick={()=>{setCnt(cnt+1)}}>increment</button>
+       <button onClick={myDcrement}>Decrement</button>
+       <button onClick={()=>{setCnt(0)}}>Reset</button>
+       </center>
+        </>
+    )
 }
 export default App;
