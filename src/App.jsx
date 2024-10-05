0f7e3776e884bebdb8418e715cb5c10376541a9f
Jitendra <jitendra01432@gmail.com>
2024-10-05 10:56:11 +0530

    useEffect multipication

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,20 @@ import { useEffect, useState } from "react";
 
 
 const App=()=>{
-const [cnt,setCnt]=useState(0)
+const [cnt,setCnt]=useState(0);
+const [multi,SetMulti]=useState(0);
 
-   useEffect(()=>{
-    setCnt(cnt+1)
-   },[])
+    useEffect(()=>{
+        SetMulti(cnt*2)
+    },[cnt])
 
     return(
         <>
         
-      <h1>wellcome to   {cnt}</h1>
-      
+            <h1>my count : {cnt}</h1>
+            <h2>multiplication : {multi}</h2>
+            <button onClick={()=>{setCnt(cnt+1)}}>click</button>
+  
         </>
     )
 }
