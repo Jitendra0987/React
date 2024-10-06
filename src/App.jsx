4ca6ecdfecb4c80f34139f4e856ccb9952ee78dd
Jitendra <jitendra01432@gmail.com>
2024-10-06 23:03:27 +0530

    from use spread oprator

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,37 @@
 import { useState } from "react";
 
+
+
 const App=()=>{
-  const [name,setName]=useState("");
-  const [city,setCity]=useState("");
-  const handle=()=>{
-    console.log({stuname:name, stucity:city})
+
+  const [input,setInput]=useState({})
+
+  const handleInput=(e)=>{
+     let name=e.target.name;
+     let value=e.target.value;
+
+
+     setInput((values)=>({...values, [name]:value}));
+  console.log(input);
+
   }
+  const handleSubmit=()=>{
+    console.log(input);
+  }
+
+  
   return(
     <>
-     <h1> Application from </h1>
-     Enter name : <input type="text" value={name} onChange={(e)=>{setName(e.target.value)}}/>
-     <br /><br />
-     Enter city : <input type="text"  value={city} onChange={(e)=>{setCity(e.target.value)}}/>
-     <br /><br />
-     <button onClick={handle}>click</button>
+    <h1>Application from</h1>
+    Enter name: <input type="text" name="stunnm" value={input.stunm} onChange={handleInput}/>
+    <br />
+    Enter city : <input type="text" name="city"  value={input.city}  onChange={handleInput}/>
+    <br />
+    Enter contact :<input type="text" name="contact" value={input.contact} onChange={handleInput}/>
+    <br />
+    Enter email : <input type="text" name="email" value={input.email} onChange={handleInput}/>
+     <br />
+     <button onClick={handleSubmit}>click here</button>
     </>
   )
 }
