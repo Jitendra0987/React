5f022b27618582ee731b0b5f14abd2e36b01c5db
Jitendra <jitendra01432@gmail.com>
2024-10-08 23:56:47 +0530

    git axios get method use

--- src/App.jsx
+++ src/App.jsx
@@ -1,142 +1,40 @@
-REACT-ROUTING(App.jsx)
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-
-import { BrowserRouter, Routes,Route } from "react-router-dom";
-import Layout from "./Layout";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
-import Nopage from "./pages/Nopage";
 
 const App=()=>{
-  return(
-    <>
-     <BrowserRouter>
-     <Routes>
-      <Route  path="/" element={<Layout/>}>
-      <Route path="home"  element={<Home/>}/>
-      <Route path="about"  element={<About/>}/>
-      <Route path="contact"  element={<Contact/>}/>
-      <Route path="*" element={<Nopage/>}/>
-      </Route>
-     </Routes>
-     </BrowserRouter>
-    </>
-  )
+    const[mydata,setdata]=useState([]);
+    const loaddata=()=>{
+        let api="http://localhost:3000/data";
+        axios.get(api).then((res)=>{
+           setdata(res.data);
+           console.log(res.data);
+        })
+    }
+    useEffect(()=>{
+      loaddata();
+         
+    },[])
+     const ans=mydata.map((key)=>{
+        <tr>
+        <td>{key.rollno}</td>
+        <td>{key.name}</td>
+        <td>{key.city}</td>
+        <td>{key.fees}</td>
+        </tr>
+     })
+    return(
+        <>
+       <h1>welcome to cybrom student</h1>
+       <table>
+        <tr>
+            <th>ROLLNO</th>
+            <th>NAME</th>
+            <th>CITY</th>
+            <th>FEES</th>
+        </tr>
+       </table>
+        </>
+    )
 }
 export default App;
-
-
-
-
-
-                            Layout page [Outlet] 
-
-import {Link,Outlet} from "react-router-dom";
-
-const Layout=()=>{
-  return(
-    <>
-    <Link to="home">Home</Link>
-    <Link to="about">About</Link>
-    <Link to="contact">Contact</Link>
-    <Outlet/>
-    www.mypage.com
-    </>
-  )
-}
-export default Layout;
-
-
-
-
-
-
-
-
-
-
-
-                            [ ROUTING WITH NESTING] 
-
-import { BrowserRouter,Routes,Route } from "react-router-dom";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import AboutCompany from "./pages/AboutCompany";
-import AboutProduct from "./pages/AboutProduct";
-import Contact from "./pages/Contact";
-import Layout from "./Layout";
-
-const App=()=>{
-  return(
-    <>
-    <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Layout/>}>
-      <Route path="home" element={<Home/>}/>
-      <Route path="about" element={<About/>}>
-      <Route path="aboutcompany" element={<AboutCompany/>}/>
-      <Route path="aboutproduct" element={<AboutProduct/>}/>
-      </Route>
-      <Route path="contact"   element={<Contact/>}/>
-      </Route>
-    </Routes>
-    </BrowserRouter>
-    </>
-  )
-}
-export default App;
-
-
-
-import {Link,Outlet} from "react-router-dom"
-
-const Layout=()=>{
-  return(
-    <>
-    <Link to="home">Home</Link>
-    <Link to="about">About</Link>
-    <Link to="contact">Contact</Link>
-    <Outlet/>
-    www.www.www
-    </>
-  )
-}
-export default Layout;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-import {Link,Outlet} from "react-router-dom"
-
-const About=()=>{
-  return(
-    <>
-    <h1>home page</h1>
-      <Link to="aboutproduct">Aboutproduct</Link>
-      <Link to="aboutcompany">AboutCompany</Link>
-      <Outlet/>
-    </>
-  )
-}
-export default About;
-
-
-
-
-
-
-
-
-
-
